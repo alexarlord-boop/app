4044015c95480a726a8c40f58f87c71e749c5404
alexarlord-boop <rezerw0.1@yandex.ru>
2023-05-15 17:43:27 +0300

    ANDROID-1-2023 Added Date related methods in IOUtils.kt

--- app/src/main/java/com/example/app/IOUtils.kt
+++ app/src/main/java/com/example/app/IOUtils.kt
@@ -1,8 +1,28 @@
 package com.example.app
+import com.google.gson.Gson
 import java.io.File
 import java.io.IOException
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class IOUtils {
+
+    val FORMAT = "dd.MM.yyyy"
+    fun convertDateToFormattedString(date: LocalDateTime): String {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern(FORMAT)
+        return date.format(dateTimeFormatter)
+    }
+
+    fun convertStringToDate(date: String): LocalDateTime {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern(FORMAT)
+        return LocalDate.parse(date, dateTimeFormatter).atStartOfDay()
+    }
+
+    fun listToJson(list: List<ServerRecord>): String {
+        val gson = Gson()
+        return gson.toJson(list)
+    }
 
     fun saveJsonToFile(jsonString: String, filePath: String) {
         val file = File(filePath)
