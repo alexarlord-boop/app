a86f2c6a1de9f10b040333da1021688304e1fbb4
alexarlord-boop <rezerw0.1@yandex.ru>
2024-02-16 13:40:47 +0200

    ANDROID-1-2024 Deleted legacy imports in StatementDialog.kt

--- app/src/main/java/com/example/app/StatementDialog.kt
+++ app/src/main/java/com/example/app/StatementDialog.kt
@@ -1,72 +1,22 @@
 package com.example.app
-
-
-import android.Manifest
-import android.content.Context
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Bundle
-import android.util.Log
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowUpward
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.MoveUp
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.lifecycle.*
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import androidx.savedstate.SavedStateRegistryOwner
 import com.example.app.data.*
-import com.example.app.navigation.Screen
-import com.example.app.record.RecordDto
-import kotlinx.coroutines.*
-import java.io.File
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.*
-import com.example.app.data.*
-import kotlinx.coroutines.*
+
 
 @Composable
 fun StatementItem(
@@ -263,16 +213,82 @@ fun ComposeView(content: @Composable (Modifier) -> Unit) {
 @Composable
 fun StatementDialogPreview() {
     val statements = remember {
-        mutableStateOf(listOf(
-            RecordStatement("1", "2024-02-06", "Source1", "StaffLink1", "StaffName1", "CompanyLink1", "Address1"),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-            RecordStatement("2", "2024-02-07", "Source2", "StaffLink2", "StaffName2", "CompanyLink2", null),
-        ))
+        mutableStateOf(
+            listOf(
+                RecordStatement(
+                    "1",
+                    "2024-02-06",
+                    "Source1",
+                    "StaffLink1",
+                    "StaffName1",
+                    "CompanyLink1",
+                    "Address1"
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+                RecordStatement(
+                    "2",
+                    "2024-02-07",
+                    "Source2",
+                    "StaffLink2",
+                    "StaffName2",
+                    "CompanyLink2",
+                    null
+                ),
+            )
+        )
     }
 
     StatementDialog(
