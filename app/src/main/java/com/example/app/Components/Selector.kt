486bcdc39abfd3707136b3bd6bba8544f832ba5a
alexarlord-boop <rezerw0.1@yandex.ru>
2024-03-10 14:50:17 +0200

    ANDROID-1-2024 Updated Selector

--- app/src/main/java/com/example/app/Components/Selector.kt
+++ app/src/main/java/com/example/app/Components/Selector.kt
@@ -1,7 +1,6 @@
 package com.example.app.Components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -13,22 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.app.data.Branch
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.reflect.typeOf
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun <T> Selector(
     selectedValue: T?,
-    initLabel: String = "филиал",
+    label: String,
     options: List<T>,
     getLabel: (T) -> String,
     onValueSelected: (T) -> Unit,
@@ -47,9 +45,15 @@ fun <T> Selector(
                 modifier = modifier,
                 border = BorderStroke(1.dp, color = Color.Black),
                 colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
-                onClick = {}) {
-                val header = if (selectedValue != null) getLabel(selectedValue) else initLabel
-                Text(header, textAlign = TextAlign.Center)
+                onClick = {})
+            {
+                selectedValue?.let{
+                    Text(text =  getLabel(it), textAlign = TextAlign.Center)
+                }
+                run {
+                    Text(text = label, textAlign = TextAlign.Center)
+                }
+
             }
 
             ExposedDropdownMenu(
@@ -73,7 +77,8 @@ fun <T> Selector(
 @Composable
 fun BranchSelectorPreview() {
     Selector(
-        selectedValue = "test branch",
+        selectedValue = null,
+        label = "Some init label",
         options = listOf("Option 1", "Option 2", "Option 3"),
         onValueSelected = { /* Handle selected value */ },
         getLabel = {it},
